2dfbfa24a20ae812f771a182252b5c3dab8c188a
pjaaaay <boruettpj@gmail.com>
2024-04-28 23:12:01 +0300

    more server changes

--- src/server.py
+++ src/server.py
@@ -2,10 +2,10 @@
 from flask import Flask,jsonify
 import os
 
-app= Flask(__name__)
+server= Flask(__name__)
 
 
-@app.route('/')
+@server.route('/')
 def index():
 
     html_content="""" <!DOCTYPE html>
@@ -24,17 +24,17 @@ def index():
       
     return html_content,200
 
-@app.route('/home',methods=['GET'])
+@server.route('/home',methods=['GET'])
 def home():
 
     server_id =os.getenv('SERVER_ID','0')
     return jsonify(message=f'Hello from Server:{server_id}',status='successful'),200
 
-@app.route('/heartbeat',methods=['GET'])
+@server.route('/heartbeat',methods=['GET'])
 def heartbeat():
     return'',200
     
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000)
+    server.run(host='0.0.0.0',port=5000)
 
